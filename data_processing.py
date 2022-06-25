be8e1e7b8f9c0a9cb0697df40cbf1855e951cfe4
Sun0023 <sunkannah346@gmail.com>
2022-06-25 09:28:25 +0530

    Staged it

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,9 @@
 #We need correct file name
-print("Take this file to the gitHub")
+print("Take this file to the gitHub") 
+a= 5
+b = 7
+def sum(a,b):
+    c = a+b
+    return a
+
+print(" The faulty function")
